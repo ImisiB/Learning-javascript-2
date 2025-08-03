6bff12ef03bee689f5e21bb10c587da8f7faa38c
Imisi Bailey <imisibailey@gmail.com>
2025-08-03 19:15:25 +0100

    Learning Javascript

---
+++ exercises/javascript/exercise5.js
@@ -0,0 +1,14 @@
+const name = 'Basit';
+console.log(`My name is: ${name}`);
+
+const cost = 5 + 3 + 3 + 9;
+
+console.log(`Cost of food: $${cost}`);
+
+tax = cost * 0.1;
+
+console.log(`Tax (10%): $${tax}`);
+
+totalCost = cost + tax;
+
+console.log(`Total cost: $${totalCost}`);
