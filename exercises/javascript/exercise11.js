f427c169001a108ae5fa85b6a211d73679b32d62
Imisi Bailey <imisibailey@gmail.com>
2025-08-09 21:13:10 +0100

    Learning javascript

--- exercises/javascript/exercise11.js
+++ exercises/javascript/exercise11.js
@@ -81,3 +81,98 @@ function addOne(array) {
 
 console.log(addOne([1, 2, 3]));
 console.log(addOne([-2, -1, 0, 99]));
+
+function addNum(array, num) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    result.push(array[i] + num);
+  }
+
+  return result;
+}
+
+console.log(addNum([1,2,3],2));
+console.log(addNum([1,2,3],3));
+console.log(addNum([-2,-1,0,99],2));
+
+
+
+function addArrays(array1, array2) {
+  const result = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    result.push(array1[i] + array2[i]);
+  }
+
+  return result;
+}
+
+console.log(addArrays([1,1,2], [1,1,3]));
+
+
+
+function countPositive(nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result++;
+    }
+  }
+
+  return result;
+}
+
+console.log(countPositive([1, -3, 5]));
+console.log(countPositive([-2, 3, -5, 7, 10]));
+
+
+
+function minMax(nums) {
+  const result = {
+    min: nums[0],
+    max: nums[0]
+  };
+
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+
+    if (value < result.min) {
+      result.min = value;
+    }
+    if (value > result.max) {
+      result.max = value;
+    }
+
+    if (nums === '') {
+      result.min = null;
+    }
+  }
+
+  return result;
+}
+
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7, 10]));
+console.log(minMax(['']))
+
+
+
+function countWords(words) {
+  const result = {};
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (!result[word]) {
+      result[word] = 1;
+    } else {
+      result[word]++;
+    }
+  }
+
+  return result;
+}
+
+console.log(countWords(['apple', 'grape', 'apple', 'apple']));
--- javascript/11-todo-list.js
+++ javascript/11-todo-list.js
@@ -1,18 +1,26 @@
-let todoList = [];
+let todoList = []
 
-// renderTodoList();
+renderTodoList();
 
 function renderTodoList() {
 
   let todoListHTML = '';
 
   for (let i =0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = 
+    `<div>${name}</div
+    <div>${dueDate}</div>
+    <button onclick="
+      todoList.splice(${i}, 1);
+      renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    </p>`;
     todoListHTML += html;
   }
-
-  console.log(todoListHTML);
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
@@ -21,8 +29,16 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input')
   const name =  inputElement.value
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input')
+
+  const dueDate = dateInputElement.value
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    name,
+    dueDate
+  });
 
   inputElement.value = '';
 
